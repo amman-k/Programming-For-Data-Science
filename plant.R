87c362d4adfd5ccc012c85fced77f23a5276cfc9
Amman <56269856+amman-k@users.noreply.github.com>
2026-04-01 21:45:14 +0530

    Create scatter plot for plant growth analysis
    Add scatter plot visualization for plant growth data.

---
+++ plant.R
@@ -0,0 +1,17 @@
+
+
+library(ggplot2)
+
+df <- data.frame(
+  Plant_ID = 1:5,
+  Growth_Rate = c(3.2, 1.8, 2.6, 0.9, 3.8),
+  Condition_Label = c("Sunny", "Shade", "Rainy", "Drought", "Cloudy")
+)
+
+ggplot(df, aes(x = Plant_ID, y = Growth_Rate)) +
+  geom_point(color = "dodgerblue", size = 4) +
+  geom_text(aes(label = Condition_Label), vjust = 1.5, color = "darkorange") +
+  labs(title = "Scatter Plot of Plant Growth Data",
+       x = "Plant ID",
+       y = "Growth Rate")
+
